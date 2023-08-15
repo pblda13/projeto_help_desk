7a583fba8e77fbd05873e0374fbec496a1219fe2
Pâmela Borges <pamela_borges18@hotmail.com>
2023-08-15 14:15:28 -0300

    Tipos enumerados criados

---
+++ src/main/java/com/pamela/helpdesk/domain/enums/Status.java
@@ -0,0 +1,43 @@
+package com.pamela.helpdesk.domain.enums;
+
+public enum Status {
+	
+ABERTO(0,"ABERTO"),ANDAMENTO(1,"ANDAMENTO"),ENCERRADO(2,"ENCERRADO");
+	
+	private Integer codigo;
+	private String descricao;
+	
+	
+	private Status(Integer codigo, String descricao) {
+		this.codigo = codigo;
+		this.descricao = descricao;
+	}
+
+
+	public Integer getCodigo() {
+		return codigo;
+	}
+
+
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	
+	public static Perfil TOEnum(Integer cod) {
+		
+		if (cod == null) {
+			return null;
+		}
+		
+		for(Perfil x : Perfil.values()) {
+			
+			if(cod.equals(x.getCodigo())) {
+				return x;
+			}
+		}
+		throw new IllegalArgumentException("Status invalido");
+	}
+
+
+}
